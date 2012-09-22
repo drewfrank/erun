bfb835616a74e743713c5bf10b13ac187cd2eaf3
Drew Frank <drewfrank@gmail.com>
2012-09-21 17:40:19 -0700

    add programmatic interface to equery that returns a dataframe.

--- erun/equery.py
+++ erun/equery.py
@@ -2,6 +2,7 @@
 """
 Output the list of result files matching a metadata filter.
 """
+__all__ = ['query']
 
 import argparse
 import json
@@ -31,6 +32,80 @@ def select(entry, solo_flags, arg_flags):
             for k,vs in arg_flags.iteritems()):
         return False
     return True
+
+def query(result_dir, process=None, *args, **kwargs):
+    """Return a list of results (or files) matching a metadata filter.
+    
+    Parameters
+    ----------
+    result_dir : str
+        Directory containing the results and metadata file.
+    process : function, optional
+        A function to load and process each data file.
+    *args : str, optional
+        Flags to use as a filter, e.g. "-x".
+    **kwargs : optional
+        Flag/argument pairs to use as a filter, e.g. "foo='bar'".
+        The same special value types (set, range, etc.) as used with the
+        command-line erun program are accepted here.
+
+    Returns
+    -------
+    results : list
+        If the `process` argument was passed, return a list of the values
+        returned by the provided function. Otherwise, just return a list
+        of file names.
+    """
+    import pandas as pd
+    # Get the keyword argument values into the expected format (lists of strings).
+    for k,v in kwargs.iteritems():
+        if isinstance(v, basestring):
+            # v is a string. Pass it through the parsing function.
+            kwargs[k] = erun.process_arg(v)
+        elif hasattr(v, '__iter__'):
+            # v is a Python list, array, etc.
+            kwargs[k] = map(str, v)
+        else:
+            # v is probably a single, numeric value.
+            kwargs[k] = [str(v)]
+    all = json.load(open(os.path.join(result_dir, 'METADATA')))
+    solo_flags = map(str, args)
+    arg_flags = {k: v for k,v in kwargs.iteritems()}
+    matches = [x for x in all if select(x, solo_flags, arg_flags)]
+    df = dataframe_from_metadata(matches)
+    if process:
+        df['res'] = df.outfile.apply(lambda x: os.path.join(result_dir, x)).apply(process)
+    return df
+
+def dataframe_from_metadata(metadata):
+    """Create a dataframe to organize metadata.
+    
+    Parameters
+    ----------
+    metadata : list of dicts
+        Metadata entries for a collection of trials.
+
+    Returns
+    -------
+    df : pandas.DataFrame
+        A dataframe representation of the metadata. This dataframe is constructed as
+        follows:
+        - Create columns for cmd, each element of solo_flags, each element of arg_flags,
+          infile, and outfile.
+        - Columns corresponding to a solo_flag element are boolean.
+        - Columns corresponding to an arg_flag pair are string valued.
+    """
+    import pandas as pd
+    solo_cols = list(set.union(*(set(entry['solo_flags']) for entry in metadata)))
+    arg_cols = list(set.union(*(set(entry['arg_flags'].keys()) for entry in metadata)))
+    def mk_row(entry):
+        # Create a dict corresponding to one row of the DataFrame.
+        row = {col: None for col in arg_cols}
+        row.update(entry['arg_flags'])
+        row.update({col: (True if col in entry.solo_flags else False) for col in solo_cols})
+        row.update({'cmd': entry['cmd'], 'infile': entry['infile'], 'outfile': entry['outfile']})
+        return row
+    return pd.DataFrame([mk_row(x) for x in metadata])
 
 if __name__ == '__main__':
     main()
