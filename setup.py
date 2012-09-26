c9580587a6a58019015b5649326cd3592e4588e3
Drew Frank <drewfrank@gmail.com>
2012-09-25 22:41:10 -0700

    update old references to README.rst.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name = 'erun',
     version = '0.1',
     description = 'A simple experiment runner.',
-    long_description = open('README.rst').read() + '\n\n' + open('HISTORY.rst').read(),
+    long_description = open('README.md').read() + '\n\n' + open('HISTORY.rst').read(),
     author = 'Drew Frank',
     author_email = 'drewfrank@gmail.com',
     packages = [
